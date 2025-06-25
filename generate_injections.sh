52659113adfb9afc2ef9623ca4755e1b81135b10
tsyamamoto@pegasus <tak.guruguru.4@gmail.com>
2025-06-25 11:19:12 +0900

    Update various things for the use of clusters

---
+++ generate_injections.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -l elapstim_req=20:00:00
+#PBS -q gpu
+#PBS -A ML4GW
+#PBS -o generate_injections.out
+#PBS -e generate_injections.out
+
+DATASETTYPE=4
+SEEDNUMBER=256
+OUTPUTDIR=data/mdc/ds4/
+OUTPUT_INJECTION_FILE=$OUTPUTDIR/injection.hdf
+OUTPUT_FOREGROUND_FILE=$OUTPUTDIR/foreground.hdf
+DURATION=2592000
+
+set -x
+module load cuda/12.1.0
+
+cd $PBS_O_WORKDIR
+apptainer exec --nv --bind `pwd` dl4longcbc.sif ./mdc/generate_data.py\
+	-d $DATASETTYPE\
+	-i $OUTPUT_INJECTION_FILE\
+	-f $OUTPUT_FOREGROUND_FILE\
+	-s $SEEDNUMBER\
+	--duration $DURATION\
+	--verbose
+cp generate_injections.sh $OUTPUTDIR
+cp generate_injections.out $OUTPUTDIR
